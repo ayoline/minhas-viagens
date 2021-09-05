8ffdcd0e1c494dcd88fc339316fbd2e991173203
ayoline <ayo@outlook.com.br>
2021-09-04 21:15:46 -0300

    configura o marcador no mapa para exibir a Rua do mesmo

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Projeto em Flutter usado para estudar e testar alguns recursos da linguagem.
 ## Plugins utilizados no App:
 
 - google_maps_flutter: ^2.0.8
+- geolocator: ^7.6.0
+- geocoding: ^2.0.1
 
--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 class Mapa extends StatefulWidget {
   const Mapa({Key? key}) : super(key: key);
@@ -12,6 +14,17 @@ class Mapa extends StatefulWidget {
 class _MapaState extends State<Mapa> {
   Completer<GoogleMapController> _controller = Completer();
   Set<Marker> _marcadores = {};
+  CameraPosition _posicaoCamera = CameraPosition(
+    target: LatLng(-11.098188694141436, -37.13673762628679),
+    zoom: 18,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    _adicionarListenerLocalizacao();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +36,7 @@ class _MapaState extends State<Mapa> {
         child: GoogleMap(
           markers: _marcadores,
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-11.098188694141436, -37.13673762628679),
-            zoom: 18,
-          ),
+          initialCameraPosition: _posicaoCamera,
           onMapCreated: _onMapCreated,
           onLongPress: _exibirMarcador,
         ),
@@ -34,15 +44,56 @@ class _MapaState extends State<Mapa> {
     );
   }
 
-  _exibirMarcador(LatLng latLng) {
-    Marker marcador = Marker(
-        markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
-        position: latLng,
-        infoWindow: InfoWindow(title: "Marcador"));
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
 
-    setState(() {
-      _marcadores.add(marcador);
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        _posicaoCamera,
+      ),
+    );
+  }
+
+  _adicionarListenerLocalizacao() {
+    Geolocator.getPositionStream(
+      distanceFilter: 5, // monitora o usurio para atualizar a cada 5metros
+      desiredAccuracy: LocationAccuracy.best,
+    ).listen((Position position) {
+      setState(() {
+        print("localização atual: " +
+            position.latitude.toString() +
+            " , " +
+            position.longitude.toString());
+        _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 16,
+        );
+        _movimentarCamera();
+      });
     });
+  }
+
+  _exibirMarcador(LatLng latLng) async {
+    List<Placemark> listaEnderecos = await placemarkFromCoordinates(
+      latLng.latitude,
+      latLng.longitude,
+    );
+
+    if (listaEnderecos.length > 0) {
+      Placemark endereco = listaEnderecos[0];
+      String? rua = endereco.thoroughfare;
+
+      Marker marcador = Marker(
+          markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
+          position: latLng,
+          infoWindow: InfoWindow(
+            title: rua,
+          ));
+
+      setState(() {
+        _marcadores.add(marcador);
+      });
+    }
   }
 
   _onMapCreated(GoogleMapController controller) {
